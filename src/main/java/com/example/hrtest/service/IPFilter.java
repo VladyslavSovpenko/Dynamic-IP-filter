11f2617d65542b5067d201beb58b905470fc3f65
Vladyslav <vladyslav.sovpenko@gmail.com>
2022-05-17 19:37:11 +0300

    add logging

--- src/main/java/com/example/hrtest/service/IPFilter.java
+++ src/main/java/com/example/hrtest/service/IPFilter.java
@@ -1,6 +1,8 @@
 package com.example.hrtest.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -12,10 +14,11 @@ import java.util.List;
 @Component
 public class IPFilter implements Filter {
 
-//    private static final Logger LOGGER = Logger.getLogger(IPFilter.class);
+    Logger LOGGER = LoggerFactory.getLogger(IPFilter.class);
 
     private IPReader reader;
     private List<String> blackList;
+    int entryTry = 0;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -25,22 +28,21 @@ public class IPFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         blackList = reader.read();
-        HttpServletRequest req = (HttpServletRequest) request;
-        int entryTry = 1;
 
+        HttpServletRequest req = (HttpServletRequest) request;
         String remoteAdr = req.getRemoteAddr();
-        System.out.println(remoteAdr);
 
         if (null == remoteAdr || remoteAdr.trim().equals("")) {
-            throw new RuntimeException("IP The address is empty.,access denied!");
+            throw new RuntimeException("IP The address is empty,access denied!");
         }
 
         if (!comparingIp(remoteAdr)) {
-//            LOGGER.info("Try to connect user in blacklist with ip= " + remoteAdr + ".  " + entryTry + " attempt blocked.");
+
             request.setAttribute("answer", "Access allowed");
             chain.doFilter(request, response);
         } else {
             entryTry++;
+            LOGGER.error("!!!Try to connect user in blacklist with ip = " + remoteAdr + ". " + entryTry + " attempt blocked.!!!");
             request.setAttribute("answer", "Access disallowed");
             chain.doFilter(request, response);
         }
